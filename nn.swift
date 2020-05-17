82599edd268da721b2c789530cafbca3b52980bc
Andrew Pynch <andrewpynchbusiness@gmail.com>
2020-05-17 10:08:14 -0700

    swift package manager sucks

--- nn.swift
+++ nn.swift
@@ -27,46 +27,24 @@ class Neuron {
     
     init(nin: Int, nonlin: Bool=true) {
         self.w = []
-        for i in 1...nin {
+        for i in 0...nin {
             self.w.append(Value(data: (Float.random(in: -1..<1))))
         }
-        self.b = Value(data: 0)
+        self.b = Value(data: 10)
         self.nonlin = nonlin
     }
-
-    func _call(_ inputs: [Value]) -> Value {
-        // (inputs • weights) + bias
-        var wXi: [F] = []
-        for (i, weight) in n1.w.enumerated() {
-            wXi.append(weight.data + inputs[i].data)
-        }
-        // sum(inputs • weights) + bias
-        var act = Value(data: sum(wXi) + self.b.data)
-        if self.nonlin {
-            return act._relu()
-        } else {
-            return act
-        }
-    }
     
+    func _call(_ x:)
     
     func _parameters(_ show: Bool=false) {
         for weight in self.w {
             weight.data += self.b.data
             if (show) {
-                print("Weight: \(weight.data)")
+                print(weight.data)
             }
         }
     }
 }
-// var n1 = Neuron(nin: 3)
-// var inputs: [Value] = [Value(data: 1.0), Value(data: 2.0), Value(data: 2.5)]
-// n1._parameters(true)
-// n1._call(inputs).data
 
-class Layer {
-
-}
-
-// MLP = Multi Layer Perceptron
-class MPL
+var n1 = Neuron(nin: 3)
+n1.parameters(true)
